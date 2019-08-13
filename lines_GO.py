423822df514ec22db9e91981b8f304f73a978181
Lino Cesar <linux06@hotmail.com>
2019-08-13 12:17:49 -0300

    Add relatorioGC e frequenciaTaxon_Encode

--- lines_GO.py
+++ lines_GO.py
@@ -1,11 +1,14 @@
 from Bio import SeqIO as fastaReader
-from Bio import SeqUtils as statGC
+from Bio import SeqUtils as calculate
 import fnmatch as padrao
 import re as regex
 import os
+from operator import itemgetter
 
 #banco de dados (dicionário) formato fasta
 taxon_encodeID_db = {}
+taxonStat = {}
+freq_taxon_encode = {}
 
 #lista de taxons a serem analisados
 taxons = ["Aotus", "Microcebus", "Otolemur", "Pan", "Callicebus", "Homo", "Nomascus", "Callithrix"]
@@ -15,29 +18,19 @@ taxons = ["Aotus", "Microcebus", "Otolemur", "Pan", "Callicebus", "Homo", "Nomas
 #Retorna dicionário onde cada chave é composta por: TAXON_ENCODEID e sua respectiva sequência nucleotídica
 def create_Taxon_EncodeID_db(diretorio):
     
-    #caminho absoluto do diretorio onde estao os arquivos .fas
     path = os.path.abspath(diretorio)
 
-    #Laço for para cada arquivo encontrado na lista de conteudo do diretório
-    for nome_arquivo_fas in os.listdir(path):
+    for nome_arquivo_fas in os.listdir(path): 
+        
+        if padrao.fnmatch(nome_arquivo_fas, "*.fas"):   #Identifica se o arquivo listado está no formato
 
-        #Identifica se o arquivo listado está no formato .fas. Se sim, entao o arquivo é tratado no laço for abaixo
-        if padrao.fnmatch(nome_arquivo_fas, "*.fas"):
-
-            #cada fasta do arquivo .fas é tratado individualmente
-            for fasta in fastaReader.parse(path + "/" + nome_arquivo_fas, "fasta"):    #leitura de arquivo fasta
+            for fasta in fastaReader.parse(path + "/" + nome_arquivo_fas, "fasta"): #para cada fasta do arquivo .fas
                 
-                #O cabeçalho do fasta está presente na lista de taxons?
-                if fasta.id in taxons:                          
+                if fasta.id in taxons: 
                     
-                    #O encodeID é retornado do próprio nome do arquivo .fas
                     encodeID = get_encodeIDFromENCODE(nome_arquivo_fas)
-
-                    #O cabeçalho(chave do dicionario) do arquivo fasta é formado pelo fasta.id (Aotus, Pan, Homo,...) mais o ENCODEID
                     head = fasta.id + "_" + encodeID
-                    
-                    #armazena sequencia fasta de acordo com sua chave
-                    taxon_encodeID_db[head] = fasta.seq.upper()
+                    taxon_encodeID_db[head] = fasta.seq.upper() #armazena sequencia fasta de acordo com sua chave
 
     return taxon_encodeID_db
 
@@ -47,53 +40,44 @@ def create_Taxon_EncodeID_db(diretorio):
 # chamada a funcão responsável por escrever em disco os resultados encontrados.
 def createDataFromRepeatMasker(diretorio):
     
-    #caminho absoluto do diretorio dos arquivos txt do repeatMasker
+    
     path = os.path.abspath(diretorio)                       
     
-    #trata cada arquivo encontado no diretório fornecido pelo usuário
+    
     for arquivo_encode in os.listdir(path):
         
-        #Se arquivo tratado é do Repeat Masker
-        if padrao.fnmatch(arquivo_encode, "Repeat_Encode*"):
+        
+        if padrao.fnmatch(arquivo_encode, "Repeat_Encode*"):  #Se arquivo tratado é do Repeat Masker
             
-            #abre arquivo Repeat Encode para leitura
             arquivo = open(path + "/" + arquivo_encode, 'r') 
             
             for linha in arquivo:                            #ler cada linha do arquivo
-                isLine = tem_LINE_L1(linha)                  #busca por padrao LINE/L1 em cada linha, retorna TRUE ou False
+                isLine = tem_LINE_L1(linha)                  #busca por padrao LINE/L1 em cada linha, retorna TRUE/FALSE
                 
                 if isLine:                                  #Se tiver LINE
                     linha = formata_RepeatMasker(linha)     #formata linha do LINE identificado
                     it = linha.split('\t')                  #cria lista onde cada coluna da linha é um elemento definido
-                    taxon = it[4]                           #coluna 5 da linha corresponde ao taxon(Aotus, Microcebus etc)
+                    taxon = it[4]                           #coluna 5 da linha corresponde ao taxon(Aotus,Pan e etc)
                     
-                    #O taxon lido na linha esta presente na lista taxons?
-                    if taxon in taxons:
+                    if taxon in taxons:                                             #O taxon lido está presente na lista
                         encodeID = get_encodeIDFromRepeatMasker(arquivo_encode) 
-                        posicao_inicial = int(it[5])                                #posicial inicial da sequência de um LINE
-                        posicao_final = int(it[6])                                  #posicao final da sequência de um LINE
+                        posicao_inicial = int(it[5])                                #posicial inicial seq de um LINE
+                        posicao_final = int(it[6])                                  #posicao final da seq de um LINE
                         tamanho_LINE = posicao_final - posicao_inicial              #tamanho de um LINE
                         
                         if tamanho_LINE >= 5000:                                    #filtro de tamanho em pares de base
                             taxon_encodeID = get_taxon_encodeID(taxon, encodeID)    #chave de busca no taxon_encodeID_db
                             
-                            if taxon_encodeID_db.__contains__(taxon_encodeID):         #esse taxon_encodeID está no banco taxon_encodeID_db?
+                            if taxon_encodeID_db.__contains__(taxon_encodeID):      #taxon_encodeID está no banco? 
                                 
-                                #Recupera sequência nucleotídica de acordo com o ID
                                 sequencia = get_Seq_From_TaxonEncodeIDdb(taxon_encodeID)
-
-                                #Recupera a sequência nuclotídica do LINE
-                                sequencia_LINE = get_seq_LINE(sequencia, posicao_inicial, posicao_final)             
+                                sequencia_LINE = get_seq_LINE(sequencia, posicao_inicial, posicao_final)
+                                cabecalho_LINE = format_cabecalho_LINE(taxon, encodeID, posicao_inicial, posicao_final)
+                                alvo_fasta_LINE = format_fasta_LINE(cabecalho_LINE, sequencia_LINE)
                                 
-                                #Formata cabecalho do LINE
-                                cabecalho_LINE = format_cabecalho_LINE(taxon, encodeID, posicao_inicial, posicao_final) 
-                                
-                                #formata um arquivo fasta
-                                alvo_fasta_LINE = format_fasta_LINE(cabecalho_LINE, sequencia_LINE) 
-                                
-                                #salva em disco 
                                 escreverArquivoFasta(cabecalho_LINE, alvo_fasta_LINE)
-                        
+                                taxonStat[cabecalho_LINE] = calculate.GC(alvo_fasta_LINE)  
+                                updateFreqTaxonEncode(taxon, encodeID)                      
             arquivo.close()
 
 #Função: Recebe o caminho relativo do arquivo .fas e retorna apenas nome do arquivo sem a extensao:
@@ -153,11 +137,51 @@ def formata_RepeatMasker(linha_do_repeat_masker):
 #Funcao: Recebe 2 parâmetros(String, String)
 def escreverArquivoFasta(cabecalho_fasta, sequencia_nucleotidica):
     
-    filename = cabecalho_fasta + ".fasta"                       #O nome do arquivo será o mesmo do cabeçalho acresentando a extensao .fasta
+    filename = cabecalho_fasta + ".fasta"              #O nome do arquivo será o mesmo do cabeçalho acresentando a extensao .fasta
     arquivo_saida = open(filename, 'w')
     arquivo_saida.write(str(sequencia_nucleotidica))
     arquivo_saida.close()
     print("O arquivo "+ filename + " foi criado!")
+
+
+def updateFreqTaxonEncode(taxon, encode):
+    cabecalho = taxon + "," + encode
+
+    if freq_taxon_encode.__contains__(cabecalho):
+        freq_taxon_encode[cabecalho] += 1
+    else:
+        freq_taxon_encode[cabecalho] = 1
+
+def relatorioGC():
+    report = "SEQ,TAMANHO_SEQ,GC\n"
+        
+    for head, gc in taxonStat.items():
+        aux = head.split("_")
+        tamanho = int(aux[3])-int(aux[2]) + 1
+        report += "{},{},{:.2f}\n".format(head, tamanho, gc)
+    return report           
+
+def relatorioFreq():
+    report = "TAXON,ENCODE,FREQUENCIA\n"
+    sorted_freq = sorted(freq_taxon_encode.items(), key = itemgetter(1), reverse = True)
+
+    for k,v in sorted_freq:
+        report += "{},{}\n".format(k,v)
+    return report
+
+def salvarRelatorioFreq(report):
+    filename = "taxon_encode_count.csv"
+    writer = open(filename, 'w')
+    writer.write(str(report))
+    writer.close()
+    print("O arquivo " + filename + "foi gerado")
+
+def salvarRelatorioGC(report):
+    filename = "reportGC.csv"
+    writer = open(filename, 'w')
+    writer.write(str(report))
+    writer.close()
+    print("O arquivo " + filename + "foi gerado")
 
 #criacao dos diretorios para cada taxon e mover os resultados para os respectivos diretórios    
 def make():
@@ -186,3 +210,5 @@ if __name__ == '__main__':
     createDataFromRepeatMasker(diretorio_repeat)
     print("\nExtração concluída!")
     make()
+    salvarRelatorioGC(relatorioGC())
+    salvarRelatorioFreq(relatorioFreq())
